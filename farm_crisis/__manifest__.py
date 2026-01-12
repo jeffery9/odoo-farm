fb6c3ae3a938dd0c560ac415fa6518fdbeb7be95
jeffery <jeffery9@gmail.com>
2026-01-12 09:08:41 +0800

    feat(crisis): implement emergency protocol management, incident tracking, and automated weather disaster alerts (US-57, 64)

---
+++ farm_crisis/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Farm Crisis & Emergency Response',
+    'version': '1.0',
+    'category': 'Industries/Agriculture',
+    'summary': 'Emergency Protocols and Crisis Management SOPs',
+    'description': """
+        Manage agricultural emergencies and crisis response [US-57].
+        - Emergency Protocols (SOPs for Disease, Fire, Contamination)
+        - Crisis Mode Activation & Asset Lockdown
+        - Incident Logging & Reporting
+    """,
+    'author': 'Jeffery',
+    'depends': ['farm_core', 'farm_safety'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/crisis_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
