a5179f2500287a3e46a13d3bcf1125ea7a38c0cb
jeffery <jeffery9@gmail.com>
2026-01-13 17:46:03 +0800

    feat(farm_core): implement base Geofence model and polygon logic

--- farm_core/models/__init__.py
+++ farm_core/models/__init__.py
@@ -2,4 +2,4 @@ from . import farm_activity
 from . import farm_location
 from . import farm_lot
 from . import product_template
-from . import soil_analysis
+from . import soil_analysisfrom . import farm_geofence
