8f3602108304676ce469e47b0c5f492849a1c704
jeffery <jeffery9@gmail.com>
2026-01-16 23:20:12 +0800

    refactor: update farm_core module structure for ISL architecture
    - Update module structure and manifest for new ISL patterns
    - Remove legacy models that have been refactored to ISL architecture
    - Update security access rules and menu structures
    - Prepare core module for ISL-compliant model implementations

--- farm_core/__manifest__.py
+++ farm_core/__manifest__.py
@@ -11,6 +11,8 @@
         - Land Parcel Management (US-01-03)
         - Biological Asset Management (US-01-04)
         - Industry Configuration Management
+        - Biological Asset Valuation and Depreciation (US-01-07)
+        - One-Click Industry Data Package Initialization (US-01-08)
     """,
     'author': 'Jeffery',
     'depends': ['project', 'stock', 'uom', 'base_setup'],
@@ -20,12 +22,11 @@
         'data/ir_sequence_data.xml',
         'data/product_category_data.xml',
         'data/ir_cron_data.xml',
-        'views/farm_activity_views.xml',
-        'views/farm_location_views.xml',
-        'views/product_template_views.xml',
-        'views/soil_analysis_views.xml',
-        'views/farm_geofence_views.xml',
-        'views/res_config_settings_views.xml',
+        'views/biological_asset_management_views.xml',
+        'views/land_location_management_views.xml',
+        'views/activity_operation_management_views.xml',
+        'views/geofencing_management_views.xml',
+        'views/config_setup_management_views.xml',
         'views/menu.xml',
     ],
     'demo': [
@@ -34,4 +35,5 @@
     'installable': True,
     'application': False,
     'license': 'AGPL-3',
+    'post_init_hook': 'post_init_hook',
 }
