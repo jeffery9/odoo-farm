34b41e9d75aa7bb8f1b786a74d7152740c059f9f
jeffery <jeffery9@gmail.com>
2026-01-31 14:08:32 +0800

    feat(farm_ai_core): implement EvidenceAnalyzerMixin for dual-confidence based proof auditing

--- farm_ai_core/models/__init__.py
+++ farm_ai_core/models/__init__.py
@@ -1,4 +1,5 @@
 from . import ai_base
+from . import evidence_analyzer
 from .ai_services import ai_decision_engine
 from .ai_services import ai_model_service
 from .ai_services import ai_mixin_service
