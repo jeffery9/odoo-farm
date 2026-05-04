217d8239aeb637cb3810decaedf627dcedd88b5b
Jeffery CHEN fan <jeffery9@gmail.com>
2026-05-04 17:59:21 +0800

    chore: remove temporary utility scripts from root directory

--- fix_esg_menu.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-file_path = 'farm_esg/views/res_config_settings_views.xml'
-with open(file_path, 'r', encoding='utf-8') as f:
-    content = f.read()
-
-# The error says farm_esg.esg_base_menu is missing.
-# Let's see what menus are defined in farm_esg.
