890fdbca7e8aa8604467c3e3cffcbcc94f132102
Jeffery CHEN fan <jeffery9@gmail.com>
2026-05-04 10:20:16 +0800

    chore: clean up temporary python scripts

--- fix_bom_xpath_v2.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('farm_processing/views/mrp_bom_views.xml', 'r') as f:
-    content = f.read()
-
-content = content.replace("<xpath expr=\"//field[@name='operation_ids']/list/field[@name='workcenter_id']\"", "<xpath expr=\"//field[@name='operation_ids']//field[@name='workcenter_id']\"")
-content = content.replace("<xpath expr=\"//field[@name='bom_line_ids']/list/field[@name='product_id']\"", "<xpath expr=\"//field[@name='bom_line_ids']//field[@name='product_id']\"")
-
-with open('farm_processing/views/mrp_bom_views.xml', 'w') as f:
-    f.write(content)
--- fix_processing_menu.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('farm_processing/views/farm_processing_views.xml', 'r') as f:
-    content = f.read()
-
-content = content.replace('parent="farm_operation.menu_agri_campaign"', 'parent="farm_operation.menu_agricultural_campaigns"')
-
-with open('farm_processing/views/farm_processing_views.xml', 'w') as f:
-    f.write(content)
