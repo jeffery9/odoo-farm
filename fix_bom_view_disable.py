375213ce363bc85d48da06fcb150dacd974b0ede
Jeffery CHEN fan <jeffery9@gmail.com>
2026-05-04 10:20:16 +0800

    chore: clean up temporary python scripts

--- fix_bom_view_disable.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('farm_processing/views/mrp_bom_views.xml', 'r') as f:
-    content = f.read()
-
-content = content.replace('<record id="view_mrp_bom_form_inherit_farm_v2"', '<!-- <record id="view_mrp_bom_form_inherit_farm_v2"')
-content = content.replace('</record>', '</record> -->')
-
-with open('farm_processing/views/mrp_bom_views.xml', 'w') as f:
-    f.write(content)
