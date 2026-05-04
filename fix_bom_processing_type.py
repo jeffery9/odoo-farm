1754c3ff99a8b387b433c282fa2f0feb11f9d191
Jeffery CHEN fan <jeffery9@gmail.com>
2026-05-04 10:20:16 +0800

    chore: clean up temporary python scripts

--- fix_bom_processing_type.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('farm_processing/views/mrp_bom_views.xml', 'r') as f:
-    content = f.read()
-
-content = content.replace('<field name="processing_type" invisible="industry_type == \'standard\'"/>', '')
-
-with open('farm_processing/views/mrp_bom_views.xml', 'w') as f:
-    f.write(content)
--- fix_bom_xpath.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('farm_processing/views/mrp_bom_views.xml', 'r') as f:
-    content = f.read()
-
-content = content.replace("<xpath expr=\"//group[@name='main_group']\"", "<xpath expr=\"//sheet/group\"")
-
-with open('farm_processing/views/mrp_bom_views.xml', 'w') as f:
-    f.write(content)
