7fd0d61e5f798cfaae1496029fe05a94a9ef8141
Jeffery CHEN fan <jeffery9@gmail.com>
2026-05-04 17:59:21 +0800

    chore: remove temporary utility scripts from root directory

--- list_empty_pots.py
+++
@@ -1,4 +0,0 @@
-import os
-modules = [d for d in os.listdir('.') if os.path.isdir(d) and (d.startswith('farm_') or d.startswith('agri_') or d.startswith('precision_'))]
-empty_mods = [m for m in modules if os.path.exists(os.path.join(m, 'i18n', f"{m}.pot")) and os.path.getsize(os.path.join(m, 'i18n', f"{m}.pot")) == 0]
-print(",".join(empty_mods))
