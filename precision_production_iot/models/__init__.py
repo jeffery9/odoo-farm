71294ae1719ed403c2d6671783a27cf19b59db62
Jeffery CHEN fan <jeffery9@gmail.com>
2026-05-16 23:31:06 +0800

    refactor: extract precision_production_iot module to external repository
    - Moved 'precision_production_iot' to /Users/jeffery/containers/odoo17/addons/my-odoo-addons/
    - Removed from the current farm-specific repository as it is part of the generic manufacturing suite.

--- precision_production_iot/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import precision_iot_device
-from . import precision_iot_sensor
-from . import precision_iot_reading
-from . import precision_iot_command_log
-from . import precision_recipe_phase_iot
-from . import precision_recipe_control
