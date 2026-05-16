0f5b316c1891355cb30922c405e2cb441467efbe
Jeffery CHEN fan <jeffery9@gmail.com>
2026-05-16 23:30:29 +0800

    refactor: extract precision_production module to external repository
    - Moved 'precision_production' to /Users/jeffery/containers/odoo17/addons/my-odoo-addons/
    - Removed from the current farm-specific repository as it acts as a generic manufacturing module.

--- precision_production/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import precision_mixin
-from . import recipe_template
-from . import recipe_execution
-from . import mrp_production
-from . import mrp_workorder
-from . import stock_lot
-from . import precision_audit
-from . import mrp_workorder_extension
--- precision_production/models/stock_lot.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class StockLot(models.Model):
-    _inherit = 'stock.lot'
-    
-    quality_grade = fields.Selection([
-        ('premium', 'Premium'), ('standard', 'Standard'), ('fail', 'Rejected')
-    ], string="Precision Grade", default='standard')
