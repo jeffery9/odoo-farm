4fbb2c3ccd7f2cf00e665a64269c4eca59d1afd2
Jeffery CHEN fan <jeffery9@gmail.com>
2026-08-09 13:30:21 +0800

    feat(traceability): implement real-time dynamic reflection on Biological Assets

--- farm_core/tests/__init__.py
+++ farm_core/tests/__init__.py
@@ -5,5 +5,6 @@ from . import test_stock_matter_tracking
 from . import test_consolidation_fields
 from . import test_consolidation_constraints
 from . import test_weighted_average_decay
+from . import test_biological_asset_reflection
 
 from . import test_deep_coverage
