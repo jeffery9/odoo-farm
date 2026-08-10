4af494b19f4895d461bdaf9cfaa5913aa57490da
Jeffery CHEN fan <jeffery9@gmail.com>
2026-08-10 21:35:25 +0800

    fix(farm_mobile): adjust XML data loading order to load menus before views using menuitem

--- farm_mobile/__manifest__.py
+++ farm_mobile/__manifest__.py
@@ -15,11 +15,11 @@
     'data': [
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
+        'views/farm_mobile_menus.xml',
         'views/farm_checkin_views.xml',
         'views/farm_evidence_views.xml',
         'views/expert_call_views.xml',
         'views/farm_mobile_dashboard_views.xml',
-        'views/farm_mobile_menus.xml',
     ],
     'assets': {
         'web.assets_backend': [
