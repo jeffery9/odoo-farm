27891b0c85292dfab1c5194d8f5e11fa31aed1f4
Jeffery CHEN fan <jeffery9@gmail.com>
2026-08-11 03:18:24 +0800

    fix(farm_processing): add harvest_lot_ids to parent processing form view to satisfy net vegetables inherit

--- run_all_install_test.py
+++ run_all_install_test.py
@@ -12,7 +12,7 @@ print(f"Installing and testing {len(modules)} modules...")
 # Construct the docker compose command
 cmd = [
     "docker", "compose", "run", "--rm", "web", "odoo", 
-    "-d", "test_stdd_db_final_v93", 
+    "-d", "test_stdd_db_final_v94", 
     "-i", modules_str, 
     "--test-enable", 
     "--stop-after-init", 
