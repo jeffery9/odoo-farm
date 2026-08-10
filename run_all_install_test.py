4a3daeb749f7b18ace6ba4432ad9eaf7d5dbbf8b
Jeffery CHEN fan <jeffery9@gmail.com>
2026-08-11 03:54:37 +0800

    fix(farm_agricultural_processing): import ham_isl and aquatic_processing_isl in traceability init to register models

--- run_all_install_test.py
+++ run_all_install_test.py
@@ -12,7 +12,7 @@ print(f"Installing and testing {len(modules)} modules...")
 # Construct the docker compose command
 cmd = [
     "docker", "compose", "run", "--rm", "web", "odoo", 
-    "-d", "test_stdd_db_final_v97", 
+    "-d", "test_stdd_db_final_v98", 
     "-i", modules_str, 
     "--test-enable", 
     "--stop-after-init", 
