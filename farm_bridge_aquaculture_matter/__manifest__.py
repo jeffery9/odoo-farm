8c2bf821c8730467d2ec189931fed311cd595513
Jeffery CHEN fan <jeffery9@gmail.com>
2026-08-16 19:37:49 +0800

    feat(aquaculture_bridge): implement decoupled Aquaculture Matter Bridge and resolve registry write-block conflict

---
+++ farm_bridge_aquaculture_matter/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Aquaculture Matter Bridge (水产物料桥接积木)',
+    'version': '19.0.1.0.0',
+    'category': 'Agricultural/Bridge',
+    'depends': ['farm_core', 'farm_aquaculture'],
+    'data': [
+        'views/stock_matter_aquaculture_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
