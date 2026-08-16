ce086a4a4efb525fc6525896b165d228ac7f0d4d
Jeffery CHEN fan <jeffery9@gmail.com>
2026-08-16 18:56:49 +0800

    feat(bridge_fermentation): implement fermentation domain late-binding bridge

---
+++ farm_bridge_fermentation_matter/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Fermentation Matter Bridge (发酵罐物料桥接积木)',
+    'version': '19.0.1.0.0',
+    'category': 'Agricultural/Bridge',
+    'depends': ['farm_core', 'farm_fermentation'],
+    'data': [
+        'views/stock_matter_fermentation_views.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
