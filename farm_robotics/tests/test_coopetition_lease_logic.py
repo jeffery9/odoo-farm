7d4114217752f8baa4d08e9efa784605a0158c96
Jeffery CHEN fan <jeffery9@gmail.com>
2026-08-17 12:54:46 +0800

    feat(coopetition): implement request lease, release and wakeup queue flow

---
+++ farm_robotics/tests/test_coopetition_lease_logic.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# filepath: odoo-farm-dev/farm_robotics/tests/test_coopetition_lease_logic.py
+from odoo.tests.common import TransactionCase
+from odoo.tests import tagged
+from odoo import fields
+from datetime import timedelta
+
+@tagged('post_install', '-at_install')
+class TestCoopetitionLeaseLogic(TransactionCase):
+
+    def setUp(self):
+        super(TestCoopetitionLeaseLogic, self).setUp()
+        self.LocationModel = self.env['farm.location']
+        
+        # Search or create base locations & robots
+        self.location = self.LocationModel.create({
+            'name': 'BDD Field North (测试北地块)',
+            'location_type': 'field'
+        })
+        self.robot_a = self.env['farm.robot'].create({
+            'name': 'Scout Drone A',
+            'robot_type': 'scout'
+        })
+        self.robot_b = self.env['farm.robot'].create({
+            'name': 'Scout Drone B',
+            'robot_type': 'scout'
+        })
+
+    def test_01_immediate_acquisition_and_queuing(self):
+        """ Immediate lease on idle field, second request queued """
+        now = fields.Datetime.now()
+        
+        # 1. Scout-A requests lease
+        lease_a = self.env['agri.robotics.lease'].create({
+            'res_model': 'farm.location',
+            'res_id': self.location.id,
+            'robot_id': self.robot_a.id,
+            'expiration_date': now + timedelta(hours=2),
+            'state': 'draft'
+        })
+        lease_a.action_request_lease()
+        self.assertEqual(lease_a.state, 'active', "First lease on idle resource must be set to 'active'")
+
+        # 2. Scout-B requests lease during the same time
+        lease_b = self.env['agri.robotics.lease'].create({
+            'res_model': 'farm.location',
+            'res_id': self.location.id,
+            'robot_id': self.robot_b.id,
+            'expiration_date': now + timedelta(hours=1),
+            'state': 'draft'
+        })
+        lease_b.action_request_lease()
+        self.assertEqual(lease_b.state, 'queued', "Lease request on busy resource must be set to 'queued'")
+
+    def test_02_wakeup_chain_on_release(self):
+        """ Releasing lease A must wake up queued lease B """
+        now = fields.Datetime.now()
+        lease_a = self.env['agri.robotics.lease'].create({
+            'res_model': 'farm.location',
+            'res_id': self.location.id,
+            'robot_id': self.robot_a.id,
+            'expiration_date': now + timedelta(hours=2),
+            'state': 'active'
+        })
+        lease_b = self.env['agri.robotics.lease'].create({
+            'res_model': 'farm.location',
+            'res_id': self.location.id,
+            'robot_id': self.robot_b.id,
+            'expiration_date': now + timedelta(hours=1),
+            'state': 'queued'
+        })
+
+        # Release lease_a
+        lease_a.action_release()
+        self.assertEqual(lease_a.state, 'released', "Lease A must be released")
+        self.assertEqual(lease_b.state, 'active', "Lease B must be automatically advanced to 'active'")
